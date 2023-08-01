c42040498c7c68d8bc7427e45104471f1dc9ffd3
exb <radmir_96@mail.ru>
2023-08-01 13:54:49 +0500

    HomeScreen changed

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,7 +1,8 @@
 import {useState} from 'react'
-import {View, Text, Image, TouchableOpacity} from 'react-native'
+import {View, Text, Image, TouchableOpacity, TextInput} from 'react-native'
 import {BellIcon} from 'react-native-heroicons/outline'
-import {BellAlertIcon} from 'react-native-heroicons/solid'
+import {MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+import {BellSlashIcon} from 'react-native-heroicons/solid'
 
 const HomeScreen = ({navigation}) => {
   const [isPressed, setIsPressed] = useState(false)
@@ -9,14 +10,16 @@ const HomeScreen = ({navigation}) => {
   return (
     <View className="flex-1 relative bg-slate-300">
       {/* Header */}
-      <View className="flex-row justify-between items-center mx-4 mt-4">
+      <View className="flex-row justify-between items-center mt-4 mx-4">
         {/* Avatar */}
         <View className="flex-row">
           <Image source={require('../assets/images/avatar.png')} />
 
           {/* Text */}
           <View className="ml-5">
-            <Text className="text-black text-xl">Hi Ninejoe</Text>
+            <Text className="text-black text-xl tracking-widest">
+              Hi Ninejoe
+            </Text>
             <Text className="text-[#636773]">You are looking for a trip?</Text>
           </View>
         </View>
@@ -24,13 +27,24 @@ const HomeScreen = ({navigation}) => {
         {/* Icon */}
         {isPressed ? (
           <TouchableOpacity onPress={() => setIsPressed(false)}>
-            <BellAlertIcon size={25} color={'black'} />
+            <BellSlashIcon size={25} color={'black'} />
           </TouchableOpacity>
         ) : (
           <TouchableOpacity onPress={() => setIsPressed(true)}>
             <BellIcon size={25} color={'#28303F'} />
           </TouchableOpacity>
         )}
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center mx-4 mt-6">
+        <View className="absolute z-10 p-2">
+          <MagnifyingGlassIcon size={35} color={'#B3B4BA'} />
+        </View>
+        <TextInput
+          placeholder="Search"
+          className="bg-white w-full rounded-lg text-lg text-[#B3B4BA] pl-12 font-semibold"
+        />
       </View>
     </View>
   )
